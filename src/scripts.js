1b3ad2dd9b7d784fb861d5ecd2a4f5a640d0a27f
Caitlin Cradick <caitlincradick@gmail.com>
2023-06-09 14:09:31 -0700

    Feat/ add scritps file with imports/exports

--- src/scripts.js
+++ src/scripts.js
@@ -6,6 +6,8 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
+import {calculateTotalSpent, showBookings } from '../src/featureCode';
+import {customersTestData,bookingsTestData,roomsTestData } from '../src/test-data.js';
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
